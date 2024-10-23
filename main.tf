456848fffc3c69135b75421bba070dd4991940b5
sgandeplli <sgandepalli@softility.com>
2024-10-23 15:42:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  project = "primal-gear-436812-t0"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "ansible"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+              #! /bin/bash
+              sudo yum install -y httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              EOF
+}
